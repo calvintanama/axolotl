c0ed4ad6cb0ccbde27c3e4a606b11ef0743b56d3
Calvin Tanama - Betr. Alina <ctanama@i14s38.anthropomatik.kit.edu>
2025-01-25 20:57:43 +0000

    Add slurm jobs to run the training

---
+++ jobs/train_phi3_a100_1.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --nodes=1
+#SBATCH --time=02:00:00
+#SBATCH --gres=gpu:1
+#SBATCH --partition=gpu_4_a100
+#SBATCH --job-name=ctanama-eval
+#SBATCH --constraint=LSDF
+#SBATCH --ntasks=1 ### maybe 2 or 4
+#SBATCH --mail-user="calvin.tanama@student.kit.edu"
+#SBATCH --mail-type="ALL"
+#SBATCH --output=/home/kit/anthropomatik/cd7437/dump/slurm/train/%x_%j.out      ### Slurm Output file, %x is job name, %j is job id
+#SBATCH --error=/home/kit/anthropomatik/cd7437/dump/slurm/train/%x_%j.err       ### Slurm Error file, %x is job name, %j is job id
+user_dir="/home/kit/anthropomatik/cd7437"
+cd "$user_dir"
+### Load conda environment
+source .bashrc
+conda activate llmpruning_gcc
+### Change to the working directory
+cd llmpruning/axolotl
+### Run training
+accelerate launch --num_processes 1 -m axolotl.cli.train ./config_run/phi3_pruned_extra_pretrain_a100_1.yaml
