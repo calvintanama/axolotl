9785b7a09ddab60a7720d76d124d721fb9a0ddaa
Calvin Tanama - Betr. Alina <ctanama@i14s38.anthropomatik.kit.edu>
2025-02-08 13:38:52 +0000

    hange GPU usage from 4 to 1

--- jobs/train_phi3_a100_4_22_29_resume.sh
+++ jobs/train_phi3_a100_4_22_29_resume.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --nodes=1
 #SBATCH --time=06:00:00
-#SBATCH --gres=gpu:4
+#SBATCH --gres=gpu:1
 #SBATCH --partition=accelerated
 #SBATCH --job-name=ctanama-train
 #SBATCH --constraint=LSDF
