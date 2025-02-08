236ddc56ea25d294535d838159c9ba4cc97b9389
Calvin Tanama - Betr. Alina <ctanama@i14s38.anthropomatik.kit.edu>
2025-02-08 13:24:18 +0000

    Fix num_processes to 1

--- jobs/train_phi3_a100_4_22_29_resume.sh
+++ jobs/train_phi3_a100_4_22_29_resume.sh
@@ -21,4 +21,4 @@ cd "$workspace_dir"
 cd axolotl
 ### Run training
 unset LD_LIBRARY_PATH
-accelerate launch -m axolotl.cli.train ./config_run/phi3_pruned_extra_pretrain_22_29_a100_4_resume.yaml
+accelerate launch --num_processes 1 -m axolotl.cli.train ./config_run/phi3_pruned_extra_pretrain_22_29_a100_4_resume.yaml
