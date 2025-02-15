a91599d04a91dc6c6287df560376f7e2cc14e69b
Calvin Tanama - Betr. Alina <ctanama@i14s38.anthropomatik.kit.edu>
2025-02-15 11:33:29 +0000

    Change job name to distiguish training jobs

--- jobs/train_phi3_22_29_residual_a100_4.sh
+++ jobs/train_phi3_22_29_residual_a100_4.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=48:00:00
 #SBATCH --gres=gpu:4
 #SBATCH --partition=accelerated
-#SBATCH --job-name=ctanama-train
+#SBATCH --job-name=residual-normal-ctanama
 #SBATCH --constraint=LSDF
 #SBATCH --ntasks=1 ### maybe 2 or 4
 #SBATCH --mail-user="calvin.tanama@student.kit.edu"
